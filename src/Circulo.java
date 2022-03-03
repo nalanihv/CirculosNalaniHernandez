19609ee981f57b343af6fdd81d9abd49d23afc06
nalanihv <nalanihevi@gmail.com>
2022-03-03 11:59:09 -0600

    Creacion de getter y setter

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,2 +1,16 @@
 public class Circulo {
+float radio;
+
+/*area:pi por radio al cuadrado
+diametro: radio por dos
+circunferencia pi por diametro  */
+
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
 }
