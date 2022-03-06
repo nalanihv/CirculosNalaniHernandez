fb719cf51529395440ac31ad90e5c7c5d559cc62
nalanihv <100865881+nalanihv@users.noreply.github.com>
2022-03-06 10:28:10 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
         System.out.println("ingrese el radio");
         System.out.println();//private
+        
+        Circulos Radio= new Circulos();
+        Circulos radio= new Circulos(Radio.radio,22.3f,3,2 );
+        Radio.setRadio(leer.nextFloat());
+        Radio.getArea();
+        Radio.getCircunferencia();
+        Radio.getDiametro();
 
+        
+        
     }
 }
